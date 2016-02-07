76eacff2929d91a2642896f41733736615fd3c38
Tej Sidhu <tejssidhu@gmail.com>
2016-02-07 20:14:57 +0000

    Logon tests

--- Phonebook.WebApi.Tests/LogonTests.cs
+++ Phonebook.WebApi.Tests/LogonTests.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Phonebook.Domain.Tests
+namespace Phonebook.WebApi.Tests
 {
 	[TestClass]
-	public class UnitTest1
+	public class LogonTests
 	{
 		[TestMethod]
 		public void TestMethod1()
